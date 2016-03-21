f42943494612e0f8a01402c1399e5429e518081c
Benjamin Gerber <benjamin.gerber@camptocamp.com>
2016-03-21 17:09:55 +0100

    Update GitHub pages

--- style_search_overlay/examples/contribs/gmf/search.js
+++ style_search_overlay/examples/contribs/gmf/search.js
@@ -32,41 +32,18 @@ app.MainController = function(gmfThemes) {
     url: 'https://geomapfish-demo.camptocamp.net/2.0/wsgi/fulltextsearch'
   }];
 
+  var fill = new ol.style.Fill({color: [255, 255, 255, 0.6]});
+  var stroke = new ol.style.Stroke({color: [255, 0, 0, 1], width: 2});
   /**
-   * Style for search results
-   * @param {ol.Feature} feature The searched feature.
-   * @param {number} resolution The current resolution of the map.
-   * @return {ol.style.Style} A style for this kind of features.
+   * @type {Object.<string, ol.style.Style>} Map of styles for search overlay.
    * @export
    */
-  this.searchStyle = function(feature, resolution) {
-    var stroke, fill, image;
-    var kind = feature.get('layer_name');
-    switch (kind) {
-      case 'osm':
-        fill = new ol.style.Fill({color: [255, 255, 255, 0.6]});
-        image = new ol.style.Circle({fill: fill, radius: 5, stroke: stroke});
-        stroke = new ol.style.Stroke({color: [50, 150, 200, 1], width: 2});
-        break;
-      case gmf.COORDINATES_LAYER_NAME:
-        image = new ol.style.RegularShape({
-          stroke: new ol.style.Stroke({color: [0, 0, 0, 0.7], width: 2}),
-          points: 4,
-          radius: 8,
-          radius2: 0,
-          angle: 0
-        })
-        break;
-      default:
-        fill = new ol.style.Fill({color: [255, 255, 255, 0.6]});
-        image = new ol.style.Circle({fill: fill, radius: 5, stroke: stroke}),
-        stroke = new ol.style.Stroke({color: [255, 0, 0, 1], width: 2});
-    }
-    return new ol.style.Style({
+  this.searchStyles = {
+    'osm': new ol.style.Style({
       fill: fill,
-      image: image,
+      image: new ol.style.Circle({fill: fill, radius: 5, stroke: stroke}),
       stroke: stroke
-    });
+    })
   };
 
 
