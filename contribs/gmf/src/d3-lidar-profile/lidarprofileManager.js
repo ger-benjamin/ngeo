0baf61db8bc4ba849308ae9968a3a84d2dc8fe74
Benjamin Gerber <benjamin.gerber@camptocamp.com>
2018-02-06 09:06:55 +0100

    lidarprofileManager: use class and transform to service

--- contribs/gmf/src/d3-lidar-profile/lidarprofileManager.js
+++ contribs/gmf/src/d3-lidar-profile/lidarprofileManager.js
@@ -1,30 +1,51 @@
-goog.provide('gmf.lidarProfile');
+goog.provide('gmf.lidarProfileManager');
 
-/**
-* Provides a D3js component to be used to draw an lidar point cloud
-* profile chart.
-* Requires access to a Pytree webservice: https://github.com/sitn/pytree
-*
-* @constructor
-* @param {Object} options from pytree
-*/
 
-gmf.lidarProfile = function(options) {
+gmf.lidarProfileManager = class {
+
+ /**
+  * Provides a service to manage a D3js component to be used to draw an lidar point cloud profile chart.
+  * Requires access to a Pytree webservice: https://github.com/sitn/pytree
+  *
+  * @struct
+  * @constructor
+  * @param {angular.$http} $http Angular http service.
+  * @ngInject
+  * @ngdoc service
+  * @ngname gmfDataSourcesManager
+  */   
+  constructor($http) {
+
+    /**
+     * @type {angular.$http}
+     * @private
+     */
+    this.$http_ = $http;
+
+    /**
+     * @type {gmf.lidarProfile.plot}
+     */
+    this.plot = null;
+  
+    /**
+     * @type {gmf.lidarProfile.loader}
+     */
+    this.loader = null;
+  
+    /**
+     * @type {gmf.lidarProfile.measure}
+     */
+    this.measure = null;
+  };
 
   /**
-  * @type {gmf.lidarProfile.plot}
-  */
-  this.plot = new gmf.lidarProfile.plot(options, this);
-
-  /**
-  * @type {gmf.lidarProfile.loader}
-  */
-  this.loader = new gmf.lidarProfile.loader(options, this.plot);
-
-  /**
-  * @type {gmf.lidarProfile.measure}
-  */
-  this.measure = new gmf.lidarProfile.measure(options, this);
-
-
+   * @param {Object} options from pytree
+   */
+  init(options) {
+    this.plot = new gmf.lidarProfile.plot(options, this);
+    this.loader = new gmf.lidarProfile.loader(options, this.plot);
+    this.measure = new gmf.lidarProfile.measure(options, this);
+  };
 };
+
+gmf.module.service('gmfLidarProfileManager', gmf.lidarProfileManager);
